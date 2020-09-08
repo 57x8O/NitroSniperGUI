ab6ad667b5f0481620f4d6986af68dfc8aee030c
Peter Strick <60312421+PeterStrick@users.noreply.github.com>
2020-09-08 18:00:08 +0200

    Version 2.3.1.0

--- NitroSniperGUI/My Project/AssemblyInfo.vb
+++ NitroSniperGUI/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' indem Sie "*" wie unten gezeigt eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.3.0.0")>
-<Assembly: AssemblyFileVersion("2.3.0.0")>
+<Assembly: AssemblyVersion("2.3.1.0")>
+<Assembly: AssemblyFileVersion("2.3.1.0")>
